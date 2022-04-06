fa542dba5f44fa202681a94a4843dfb50479ddaf
Jon McCarty <mccarty.jon@gmail.com>
2022-04-06 17:29:11 -0400

    add useful comments

--- ppc.py
+++ ppc.py
@@ -6,17 +6,20 @@ from cache import cache
 from pathlib import Path
 
 class Item():
+    '''represents a person, a person has an ID and a name'''
     def __init__(self, identifier, name):
         self.item = {'id': identifier, 'name': name}
 
 
 app = Flask(__name__)
-app.config.from_file('settings.cfg', load=json.load)
+app.config.from_file('settings.cfg', load=json.load)  # settings.cfg must reside in same dir as ppc.py
 
+# create an on-disk cache. TODO replace with simple database such as redis
 cache.init_app(app=app, config={'CACHE_TYPE': 'filesystem', 'CACHE_DIR': Path('/tmp')})
 
 @app.route('/items',methods = ['POST', 'GET', 'DELETE'])
 def web_people():
+    '''handles POST, GET, and DELETE verbs'''
     try:
         if request.method == 'POST':
             '''POST requires a list of dicts, each dict contains values for "id" and "name"'''
@@ -31,8 +34,9 @@ def web_people():
             return str(people)
 
         elif request.method == 'DELETE':
+            '''DELETE clears all of the entries in the cache'''
             cache.clear()
-            return 'people have been discarded'
+            return 'the database has been cleared'
 
     except TypeError:
         return "no people"
