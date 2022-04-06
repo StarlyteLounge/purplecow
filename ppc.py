b0c4021c88cb1c374de29491913d0df0fd690ff3
Jon McCarty <mccarty.jon@gmail.com>
2022-04-06 13:44:43 -0400

    change variable to reflect contents

--- ppc.py
+++ ppc.py
@@ -1,41 +1,48 @@
+#! /usr/bin/env python3
+
 from flask import Flask, request, jsonify
 from cache import cache
 from pathlib import Path
 
 class Item():
     def __init__(self, identifier, name):
-        self.identifier = identifier
-        self.name = name
+        self.item = {'id': identifier, 'name': name}
+
 
 app = Flask(__name__)
 
 cache.init_app(app=app, config={'CACHE_TYPE': 'filesystem', 'CACHE_DIR': Path('/tmp')})
 
-@app.route('/items',methods = ['POST', 'GET', 'DELETE'])
-def web_items():
+@app.route('/people',methods = ['POST', 'GET', 'DELETE'])
+def web_people():
     try:
         if request.method == 'POST':
-            item_list = list(request.get_json())
-            items = [Item(ident,name) for ident, name in item_list]
-            print(type(items))
-            cache.set('db',items)
-            print(items)
+            '''POST requires a list of dicts, each dict contains values for "id" and "name"'''
+            people = list(request.get_json())
+            print(people)
+            #print([f'{key}: {val}' for item[0] in people.list()]
+            #print([f'{ident}: {name}') for item['id'], item] in people]
+            cache.set('db',people)
+            print(people[0].item['id'])
+            print(people[0].item['name'])
             return 'post complete'
 
         elif request.method == 'GET':
+            '''GET returns a list of dicts, each dict contains values for "id" and "name"'''
             print('processing GET')
-            items = cache.get('db')
-            print(items)
-            return str(items)
+            people = cache.get('db')
+            people = [str(item) for item in people]
+            print(people)
+            return str(people)
 
         elif request.method == 'DELETE':
             cache.clear()
             print('deleted cache:')
-            return 'items have been discarded'
+            return 'people have been discarded'
 
     except TypeError:
         print('type error')
-        return "no items"
+        return "no people"
 
 
 if __name__ == '__main__':
